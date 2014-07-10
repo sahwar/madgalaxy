79e0f27a9c9b2a51a2a0a24356ac2d68181854c8
Brady Brown <bobrown101@gmail.com>
2014-07-10 19:17:35 -0400

    validatin fixes

--- routes/index.js
+++ routes/index.js
@@ -8,40 +8,42 @@ var pageNum = 1;
 
 //Standard convention for api callback - page, variable(if applicable), callback function
 
-//
+//validating all the parameters
 router.use(function(req, res, next) {
-  pageNum = 1; //resets the pageNum to 1 - this will be changed below with router.param('pageNum') if the user specifies a val
+  pageNum = 1; //resets the pageNum to 1 - this will be changed below if the user specifies a val below
+
+  //validating pageNum
+  if(req.query.pageNum){
+    console.log('seeing if pageNum is valid');
+    if (typeof(req.query.pageNum) === "number"){
+      req.pageNum = req.query.pageNum;
+      next();
+    }else{
+      res.json({'error': 'Not a valid pageNum value'});
+      return;
+    }
+  }
+
+  //validating searchString
+  if(req.query.searchString){
+    console.log('seeing if searchString is valid');
+    if (typeof(req.query.searchString) === "string"){
+      req.searchString = req.query.searchString;
+      next();
+    }else{
+      res.json({'error': 'Not a valid searchString value'});
+      return;
+    }
+  }
+
   next(); 
 });
-
-//validates the parameter pageNum
-router.param('?pageNum', function(req, res, next, pageNum){
-  console.log('seeing if pageNum is valid');
-  if (typeof(req.query.pageNum) === "number"){
-    pageNum = req.query.pageNum;
-    next();
-  }else{
-    res.json({'error': 'Not a valid pageNum value'});
-  }
-});
-
-// //validates the parameter searchString
-// router.param('searchString', function(req, res, next, searchString){
-//   console.log('seeing if searchString is valid');
-//   if (typeof(req.query.searchString) === "string"){
-//     next();
-//   }else{
-//     res.json({'error': 'Not a valid pageNum value'});
-//   }
-// });  
-
-
 
 //request for article by id
 router.get( '/api/v1/articles/id/:article_id', function(req, res, next){
   console.log('request for single article with an id of: ' + req.params.article_id);
   var articleId = req.params.article_id;
-  apiV1.getArticleById(pageNum, articleId, function(err, post){
+  apiV1.getArticleById(req.pageNum, articleId, function(err, post){
     if(err){
       res.send(err);
     }
@@ -54,7 +56,7 @@ router.get( '/api/v1/articles/id/:article_id', function(req, res, next){
 //request for most recent articles
 router.get( '/api/v1/articles/mostRecent', function(req, res, next){
   console.log('request for most recent articles');
-  apiV1.getMostRecentArticles(pageNum, function(err, posts){
+  apiV1.getMostRecentArticles(req.pageNum, function(err, posts){
     if(err){
       res.send(err);
     }
@@ -68,7 +70,7 @@ router.get( '/api/v1/articles/mostRecent', function(req, res, next){
 router.get( '/api/v1/articles/tags/:tags', function(req, res, next){
   var paramsInArray = req.params.tags.split("+");
   console.log('request for articles categorized by tags: ' + paramsInArray);
-  apiV1.getArticlesByTags(pageNum, paramsInArray, function(err, posts){
+  apiV1.getArticlesByTags(req.pageNum, paramsInArray, function(err, posts){
     if(err){
       res.send(err);
     }
@@ -79,7 +81,7 @@ router.get( '/api/v1/articles/tags/:tags', function(req, res, next){
 });
 
 router.use('/api/v1/search/', function(req, res, next) {
-  if (req.query.searchString){
+  if (req.searchString){
     next();
   }else{
     console.log('ERROR - Empty searchString parameter');
@@ -88,9 +90,8 @@ router.use('/api/v1/search/', function(req, res, next) {
 });
 
 router.get( '/api/v1/search/', function(req, res, next){
-  var searchString = req.query.searchString;
-  console.log('search for: ' + searchString);
-  apiV1.getArticlesBySearchString(pageNum, searchString, function(err, posts){
+  console.log('search for: ' + req.searchString);
+  apiV1.getArticlesBySearchString(req.pageNum, req.searchString, function(err, posts){
     if(err){
       res.send(err);
     }
