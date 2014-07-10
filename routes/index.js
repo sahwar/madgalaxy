a2a9fcd5e8e334a6810915430311a0ae9c9edb18
Brady Brown <bobrown101@gmail.com>
2014-07-10 16:25:15 -0400

    updated how fetching single article works

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,22 @@ var express = require('express');
 var router = express.Router();
 var feedreader = require('../lib/feedreader.js');
 var apiV1 = require('../lib/api_v1.js');
+var pageNum = 1;
+// route middleware that will happen on every request
+router.use(function(req, res, next) {
+  if (req.query.pageNum){
+    pageNum = req.query.pageNum;
+  }else{
+    pageNum = 1;
+  }
+  console.log(pageNum);
+  next(); 
+});
 
 //request for single article
-router.get( '/api/v1/articles/id/*', function(req, res, next){
+router.get( '/api/v1/articles/id/:article_id', function(req, res, next){
   console.log('request for single article');
-  var articleId = req.params[0];
+  var articleId = req.params.article_id;
   console.log(articleId);
   apiV1.getArticleById(articleId, function(err, post){
     if(err){
