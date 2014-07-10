e78fa99da768b2498e51d8c52bd3fe36c18f314b
Brady Brown <bobrown101@gmail.com>
2014-07-10 17:24:29 -0400

    Send an error if there isnt a search query

--- routes/index.js
+++ routes/index.js
@@ -56,7 +56,15 @@ router.get( '/api/v1/articles/:tags', function(req, res, next){
   });
 });
 
-router.get( '/api/v1/search/', function(req, res){
+router.get( '/api/v1/search/', function(req, res, next){
+  if(req.query.searchString){
+    next();
+  }else{
+    res.json({'error': 'No search parameter'});
+  }
+});
+
+router.get( '/api/v1/search/', function(req, res, next){
   var searchString = req.query.searchString;
   console.log('the user made a search for: ' + searchString);
   apiV1.getArticlesBySearchString(searchString, function(err, posts){
