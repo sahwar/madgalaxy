b736c0d016c46e36c72ec29776d77acb47667646
Brady Brown <bobrown101@gmail.com>
2014-07-10 20:42:26 -0400

    can see untagged articles

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,7 @@ var express = require('express');
 var router = express.Router();
 var feedreader = require('../lib/feedreader.js');
 var apiV1 = require('../lib/api_v1.js');
-var pageNum = 1;
-// var perPage = 10; this is defined in api... .js
+var pageNum = 1; // this is multiplied by 'perPage', a variable defined in api...js, to determine how many articles are skipped
 
 
 //Standard convention for api callback - page, variable(if applicable), callback function
@@ -43,6 +42,18 @@ router.get( '/api/v1/articles/id/:article_id', function(req, res, next){
 router.get( '/api/v1/articles/mostRecent', function(req, res, next){
   console.log('request for most recent articles');
   apiV1.getMostRecentArticles(req.pageNum, function(err, posts){
+    if(err){
+      res.send(err);
+    }
+    else{
+      res.json(posts);
+    }
+  });
+});
+
+//request for untagged articles
+router.get( '/api/v1/articles/tags/untagged/', function(req, res, next){
+  apiV1.getUntaggedArticles(req.pageNum, function(err, posts){
     if(err){
       res.send(err);
     }
