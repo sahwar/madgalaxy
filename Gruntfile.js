bfd9f2c2e91445af5800c2a31ff3c924aca49311
Brady Brown <bobrown101@gmail.com>
2014-07-27 00:12:43 -0400

    Configured grunt"

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+// Gruntfile.js
+module.exports = function (grunt) {
+    grunt.initConfig({
+        env: {
+            options: {
+                //Shared Options Hash
+            },
+            dev: {
+                NODE_ENV: 'development',
+                DEST: 'temp',
+                DEBUG: 'server,feedreader:*,api_v1,router:*'
+            }
+        },
+        shell: {
+            mongodb: {
+                command: 'mongod --dbpath ./data',
+                options: {
+                    async: true,
+                    stdout: false,
+                    stderr: true,
+                    failOnError: true,
+                    execOptions: {
+                        cwd: '.'
+                    }
+                }
+            }
+        },
+        // configure nodemon
+        nodemon: {
+            dev: {
+                script: './bin/www'
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-env');
+    grunt.loadNpmTasks('grunt-shell-spawn');
+    grunt.loadNpmTasks('grunt-nodemon');
+    grunt.registerTask('default', ['env', 'shell', 'nodemon']);
+};
