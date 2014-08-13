dddc9d0dea4472e3fab26c8ba30d301036cf797a
Brady Brown <bobrown101@gmail.com>
2014-08-13 16:28:23 -0400

    fixed a derp

--- routes/api_v1.js
+++ routes/api_v1.js
@@ -9,7 +9,7 @@ var debug = require('debug')('router:api_v1');
 
 
 //This allows cross-domain requests
-app.all('/', function(req, res, next) {
+router.use('/', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
