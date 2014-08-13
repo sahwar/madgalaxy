3562f6956f6bdd4f55ecff9687968535a581449c
Brady Brown <bobrown101@gmail.com>
2014-08-13 16:32:06 -0400

    trying to change headers

--- routes/api_v1.js
+++ routes/api_v1.js
@@ -10,8 +10,9 @@ var debug = require('debug')('router:api_v1');
 
 //This allows cross-domain requests
 router.use('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
