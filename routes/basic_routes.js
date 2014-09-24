fc7308fc6153c6ab61f39103e5c8e286c389ad41
Brady Brown <bobrown101@gmail.com>
2014-09-24 16:04:35 -0400

    updated some success messages

--- routes/basic_routes.js
+++ routes/basic_routes.js
@@ -7,8 +7,9 @@ router.use(function (req, res, next) {
     next();
 });
 router.get('', function (req, res, next){ //this route is for www.madglalaxy.io/
-    res.json({'200':'You have reached madgalaxy.io - api is available at /api/v1/'});
+    res.json({'200':'You have reached madgalaxy.io - api is available at /api/v1/ - documentation at: https://github.com/madglory/madgalaxy'});
 });
+
 
 
 
